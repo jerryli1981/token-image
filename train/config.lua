c60dd60d21d5253d3878269e017431cbfba1eb7c
Peng Li <jerryli1981@gmail.com>
2016-02-17 21:06:57 -0600

    add train

---
+++ train/config.lua
@@ -0,0 +1,91 @@
+--[[
+Configuration for Crepe Training Program
+By Xiang Zhang @ New York University
+--]]
+
+require("nn")
+
+-- The namespace
+config = {}
+
+local alphabet = "hspnz"
+
+seq_length = 10000
+
+
+-- Training data
+config.train_data = {}
+config.train_data.file = paths.concat(paths.cwd(),"../data/train.t7b")
+config.train_data.alphabet = alphabet
+config.train_data.length = seq_length
+config.train_data.batch_size = 128
+
+-- Test data
+config.val_data = {}
+config.val_data.file =  paths.concat(paths.cwd(),"../data/test.t7b")
+config.val_data.alphabet = alphabet
+config.val_data.length = seq_length
+config.val_data.batch_size = 128
+
+-- The model
+config.model = {}
+-- #alphabet x 1014
+config.model[1] = {module = "nn.TemporalConvolution", inputFrameSize = #alphabet, outputFrameSize = 256, kW = 20}
+config.model[2] = {module = "nn.Threshold"}
+config.model[3] = {module = "nn.TemporalMaxPooling", kW = 10, dW = 10}
+-- 336 x 256
+config.model[4] = {module = "nn.TemporalConvolution", inputFrameSize = 256, outputFrameSize = 256, kW = 20}
+config.model[5] = {module = "nn.Threshold"}
+config.model[6] = {module = "nn.TemporalMaxPooling", kW = 10, dW = 10}
+-- 110 x 256
+config.model[7] = {module = "nn.TemporalConvolution", inputFrameSize = 256, outputFrameSize = 256, kW = 10}
+config.model[8] = {module = "nn.Threshold"}
+-- 108 x 256
+config.model[9] = {module = "nn.TemporalConvolution", inputFrameSize = 256, outputFrameSize = 256, kW = 10}
+config.model[10] = {module = "nn.Threshold"}
+-- 106 x 256
+config.model[11] = {module = "nn.TemporalConvolution", inputFrameSize = 256, outputFrameSize = 256, kW = 10}
+config.model[12] = {module = "nn.Threshold"}
+-- 104 x 256
+config.model[13] = {module = "nn.TemporalConvolution", inputFrameSize = 256, outputFrameSize = 256, kW = 10}
+config.model[14] = {module = "nn.Threshold"}
+config.model[15] = {module = "nn.TemporalMaxPooling", kW = 10, dW = 10}
+-- 34 x 256
+config.model[16] = {module = "nn.Reshape", size = 256 * 6}
+-- 8704
+config.model[17] = {module = "nn.Linear", inputSize = 256 * 6, outputSize = 1024}
+config.model[18] = {module = "nn.Threshold"}
+config.model[19] = {module = "nn.Dropout", p = 0.5}
+-- 1024
+config.model[20] = {module = "nn.Linear", inputSize = 1024, outputSize = 1024}
+config.model[21] = {module = "nn.Threshold"}
+config.model[22] = {module = "nn.Dropout", p = 0.5}
+-- 1024
+config.model[23] = {module = "nn.Linear", inputSize = 1024, outputSize = 5}
+config.model[24] = {module = "nn.LogSoftMax"}
+
+-- The loss
+config.loss = nn.ClassNLLCriterion
+
+-- The trainer
+config.train = {}
+local baseRate = 1e-2 * math.sqrt(config.train_data.batch_size) / math.sqrt(128)
+config.train.rates = {[1] = baseRate/1,[15001] = baseRate/2,[30001] = baseRate/4,[45001] = baseRate/8,[60001] = baseRate/16,[75001] = baseRate/32,[90001]= baseRate/64,[105001] = baseRate/128,[120001] = baseRate/256,[135001] = baseRate/512,[150001] = baseRate/1024}
+config.train.momentum = 0.9
+config.train.decay = 1e-5
+
+-- The tester
+config.test = {}
+config.test.confusion = true
+
+
+-- Main program
+config.main = {}
+config.main.eras = 1
+config.main.epoches = 2000
+config.main.randomize = 5e-2
+config.main.dropout = true
+config.main.save = paths.cwd() .. "/models"
+config.main.collectgarbage = 100
+config.main.logtime = 5
+config.main.validate = true
---
+++ train/main.lua
@@ -0,0 +1,257 @@
+--[[
+Main Driver for Crepe
+By Xiang Zhang @ New York University
+]]
+
+-- Necessary functionalities
+require("nn")
+
+-- Local requires
+require("data")
+require("model")
+require("train")
+require("test")
+require("config")
+
+require('lfs')
+
+-- Configurations
+
+-- Prepare random number generator
+math.randomseed(os.time())
+torch.manualSeed(os.time())
+
+-- Create namespaces
+main = {}
+
+-- The main program
+function main.main()
+
+   opt = main.argparse()
+   -- Setting the device
+   if opt.device > 0 then
+      require("cutorch")
+      require("cunn")
+      cutorch.setDevice(opt.device)
+      print("Device set to ".. opt.device)
+      config.main.type = "torch.CudaTensor"
+   else
+      config.main.type = "torch.DoubleTensor"
+   end
+
+   if opt.debug > 0 then
+      dbg = require("debugger")
+   end
+
+   main.clock = {}
+   main.clock.log = 0
+
+   main.new()
+   main.run()
+ 
+end
+
+
+
+-- Parse arguments
+function main.argparse()
+   local cmd = torch.CmdLine()
+
+   -- Options
+   cmd:option("-resume",0,"Resumption point in epoch. 0 means not resumption.")
+   cmd:option("-debug",0,"debug. 0 means not debug.")
+   cmd:option("-device",0,"device. 0 means cpu.")
+   cmd:text()
+
+   -- Parse the option
+   local opt = cmd:parse(arg or {})
+
+   -- Resumption operation
+   if opt.resume > 0 then
+      -- Find the main resumption file
+      local files = main.findFiles(paths.concat(config.main.save,"main_"..tostring(opt.resume).."_*.t7b"))
+      if #files ~= 1 then
+    error("Found "..tostring(#files).." main resumption point.")
+      end
+      config.main.resume = files[1]
+      print("Using main resumption point "..config.main.resume)
+      -- Find the model resumption file
+      local files = main.findFiles(paths.concat(config.main.save,"sequential_"..tostring(opt.resume).."_*.t7b"))
+      if #files ~= 1 then
+    error("Found "..tostring(#files).." model resumption point.")
+      end
+      config.model.file = files[1]
+      print("Using model resumption point "..config.model.file)
+      -- Resume the training epoch
+      config.train.epoch = tonumber(opt.resume) + 1
+      print("Next training epoch resumed to "..config.train.epoch)
+      -- Don't do randomize
+      if config.main.randomize then
+         config.main.randomize = nil
+         print("Disabled randomization for resumption")
+      end
+   end
+
+   return opt
+end
+
+-- Train a new experiment
+function main.new()
+   -- Load the data
+   print("Loading datasets...")
+   main.train_data = Data(config.train_data)
+   main.val_data = Data(config.val_data)
+   
+   -- Load the model
+   print("Loading the model...")
+   main.model = Model(config.model)
+   if config.main.randomize then
+      main.model:randomize(config.main.randomize)
+      print("Model randomized.")
+   end
+   main.model:type(config.main.type)
+   print("Current model type: "..main.model:type())
+   collectgarbage()
+
+   -- Initiate the trainer
+   print("Loading the trainer...")
+   main.train = Train(main.train_data, main.model, config.loss(), config.train)
+
+   -- Initiate the tester
+   print("Loading the tester...")
+   main.test_val = Test(main.val_data, main.model, config.loss(), config.test)
+
+   -- The record structure
+   main.record = {}
+
+   collectgarbage()
+end
+
+-- Start the training
+function main.run()
+   --Run for this number of era
+   for i = 1,config.main.eras do
+
+      if config.main.dropout then
+	     print("Enabling dropouts")
+	     main.model:enableDropouts()
+      else
+	     print("Disabling dropouts")
+	     main.model:disableDropouts()
+      end
+      print("Training for era "..i)
+      main.train:run(config.main.epoches, main.trainlog)
+
+      if config.main.validate == true then
+	     print("Disabling dropouts")
+        main.model:disableDropouts()
+	     print("Testing on develop data for era "..i)
+	     main.test_val:run(main.testlog)
+      end
+
+      print("Recording on ear " .. i)
+      main.record[#main.record + 1] = {val_error = main.test_val.e, val_loss = main.test_val.l}
+      print("Visualizing loss")
+      main.show()
+      main.save()
+      collectgarbage()
+   end
+end
+
+function main.show(figure_error, figure_loss)
+   main.figure_error = main.figure_error or gnuplot.figure()
+   main.figure_loss = main.figure_loss or gnuplot.figure()
+
+   local figure_error = figure_error or main.figure_error
+   local figure_loss = figure_loss or main.figure_loss
+
+   local epoch = torch.linspace(1, #main.record, #main.record):mul(config.main.epoches)
+   local val_error = torch.zeros(#main.record)
+   local val_loss = torch.zeros(#main.record)
+   for i = 1, #main.record do
+      val_error[i] = main.record[i].val_error
+      val_loss[i] = main.record[i].val_loss
+   end
+
+   print("val_error is")
+   print(val_error)
+
+   print("val_loss is")
+   print(val_loss)
+
+end
+
+function main.save()
+   -- Record necessary configurations
+   config.train.epoch = main.train.epoch
+
+   if lfs.attributes(config.main.save) == nil then
+         lfs.mkdir(config.main.save)
+   end
+
+   -- Make the save
+   local time = os.time()
+   torch.save(paths.concat(config.main.save,"main_"..(main.train.epoch-1).."_"..time..".t7b"),
+         {config = config, record = main.record, momentum = main.train.old_grads:double()})
+   torch.save(paths.concat(config.main.save,"sequential_"..(main.train.epoch-1).."_"..time..".t7b"),
+         main.model:clearSequential(main.model:makeCleanSequential(main.model.sequential)))
+
+   main.eps_error = main.eps_error or gnuplot.epsfigure(paths.concat(config.main.save,"figure_error.eps"))
+   main.eps_loss = main.eps_loss or gnuplot.epsfigure(paths.concat(config.main.save,"figure_loss.eps"))
+   main.show(main.eps_error, main.eps_loss)
+
+   collectgarbage()
+end
+
+-- The training logging function
+function main.trainlog(train)
+   if config.main.collectgarbage and math.fmod(train.epoch-1,config.main.collectgarbage) == 0 then
+      print("Collecting garbage at epoch = "..(train.epoch-1))
+      collectgarbage()
+   end
+
+   if (os.time() - main.clock.log) >= (config.main.logtime or 1) then
+      local msg = ""
+
+	     msg = msg.."epo: "..(train.epoch-1)..
+	    ", rat: "..string.format("%.2e",train.rate)..
+	    ", err: "..string.format("%.2e",train.error)..
+	    ", obj: "..string.format("%.2e",train.objective)
+
+      --print(msg)
+   
+      main.clock.log = os.time()
+   end
+end
+
+function main.testlog(test)
+   if config.main.collectgarbage and math.fmod(test.n,config.train_data.batch_size*config.main.collectgarbage) == 0 then
+      print("Collecting garbage at n = "..test.n)
+      collectgarbage()
+   end
+   if (os.time() - main.clock.log) >= (config.main.logtime or 1) then
+      --[[
+      print("n: "..test.n..
+	       ", e: "..string.format("%.2e",test.e)..
+	       ", l: "..string.format("%.2e",test.l)..
+	       ", err: "..string.format("%.2e",test.err)..
+	       ", obj: "..string.format("%.2e",test.objective))
+      --]]
+      main.clock.log = os.time()
+   end
+end
+
+-- Utility function: find files with the specific 'ls' pattern
+function main.findFiles(pattern)
+   require("sys")
+   local cmd = "ls "..pattern
+   local str = sys.execute(cmd)
+   local files = {}
+   for file in str:gmatch("[^\n]+") do
+      files[#files+1] = file
+   end
+   return files
+end
+
+-- Execute the main program
+main.main()
