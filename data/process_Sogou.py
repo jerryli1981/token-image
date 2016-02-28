878fb508a593b59be1a019968fec3d5a8ecf5138
Peng Li <jerryli1981@gmail.com>
2016-02-28 06:01:41 -0600

    fix bugs

--- data/process_Sogou.py
+++ data/process_Sogou.py
@@ -18,45 +18,9 @@ import codecs
 
 import itertools
 
+import random
 
-Dir = "./SogouCAS"
-
-sport_url = set()
-finance_url = set()
-ent_url = set()
-auto_url = set()
-tech_url  = set()
-
-Dic = {'Ａ':'A','Ｂ':'B','Ｃ':'C','Ｄ':'D','Ｅ':'E','Ｆ':'F','Ｇ':'G','Ｈ':'H','Ｉ':'I','Ｊ':'J','Ｋ':'K','Ｌ':'L','Ｍ':'M','Ｎ':'N','Ｏ':'O','Ｐ':'P','Ｑ':'Q',\
-'Ｒ':'R','Ｓ':'S','Ｔ':'T','Ｕ':'U','Ｖ':'V','Ｗ':'W','Ｘ':'X','Ｙ':'Y','Ｚ':'Z','ａ':'a','ｂ':'b','ｃ':'c','ｄ':'d','ｅ':'e','ｆ':'f','ｇ':'g','ｈ':'h','ｉ':'i','ｊ':'j',\
-'ｋ':'k','ｌ':'l','ｍ':'m','ｎ':'n','ｏ':'o','ｐ':'p','ｑ':'q','ｒ':'r','ｓ':'s','ｔ':'t','ｕ':'u','ｖ':'v','ｗ':'w','ｘ':'x','ｙ':'y','ｚ':'z','１':'1','２':'2','３':'3',\
-'４':'4','５':'5','６':'6','７':'7','８':'8','９':'9','０':'0','．':'.'}
-
-dot = {'。','（','）','！','「','」','，','、','；','：','”','“','～',\
-        '＜','＞','．','é','︶','『','』','﹗','ī','ō','／',"〔", '〕','｜',\
-        "？","＠","｛","｝","￥","《","》",'…','【','】','︿','＃','＄','％','＆','＊','＋','⊙','［','］',\
-       "［","］","—","·","－"}
-
-def replace_all(text, dic):
-    for i, j in dic.iteritems():
-        text = text.replace(i, j)
-    return text
-
-def seg(content):
-
-    content = content.encode('utf-8')
-    content = replace_all(content,Dic)
-
-    Cut=jieba.cut(''.join(content.split())) #斷詞
-    li = []
-    for u in Cut:
-        if u.encode('utf-8') not in dot:  #清洗全形符號
-            li.append(u) #沒在dot裡的就寫進陣列
-
-    content = (' '.join(li)).encode('utf-8') #將陣列用空白格開, 傳回字串
-
-    return content, len(li)
-
+"""
 unicode_ranges = (
         ('2E80', '2EFF'),     # CJK 部首扩展:[2E80-2EFF]
         ('2F00', '2FDF'),     # CJK 康熙部首:[2F00-2FDF]
@@ -70,6 +34,45 @@ unicode_ranges = (
         ('2F800', '2FA1F'),   # CJK 兼容扩展:[2F800-2FA1F]
     )
 
+uniset = set()
+for unicode_range in unicode_ranges:
+    for n in xrange(int(unicode_range[0], 16), int(unicode_range[1], 16) + 1):
+        uniset.add(n)
+"""
+
+
+Dic = {'Ａ':'A','Ｂ':'B','Ｃ':'C','Ｄ':'D','Ｅ':'E','Ｆ':'F','Ｇ':'G','Ｈ':'H','Ｉ':'I','Ｊ':'J','Ｋ':'K','Ｌ':'L','Ｍ':'M','Ｎ':'N','Ｏ':'O','Ｐ':'P','Ｑ':'Q',\
+'Ｒ':'R','Ｓ':'S','Ｔ':'T','Ｕ':'U','Ｖ':'V','Ｗ':'W','Ｘ':'X','Ｙ':'Y','Ｚ':'Z','ａ':'a','ｂ':'b','ｃ':'c','ｄ':'d','ｅ':'e','ｆ':'f','ｇ':'g','ｈ':'h','ｉ':'i','ｊ':'j',\
+'ｋ':'k','ｌ':'l','ｍ':'m','ｎ':'n','ｏ':'o','ｐ':'p','ｑ':'q','ｒ':'r','ｓ':'s','ｔ':'t','ｕ':'u','ｖ':'v','ｗ':'w','ｘ':'x','ｙ':'y','ｚ':'z','１':'1','２':'2','３':'3',\
+'４':'4','５':'5','６':'6','７':'7','８':'8','９':'9','０':'0','．':'.','”':'"','“':'"','。':'.','！':'!',\
+'，':',','、':',','；':';','：':':','～':'~','＜':'<','＞':'>','﹗':'!','／':'/',"〔":'[', '〕':']','｜':'|',\
+        "？":'?',"＠":'@',"｛":'{',"｝":'}',"￥":'$',"《":'<',"》":'>','…':'...','【':'[','】':']','︿':'^','＃':'#','＄':'$','％':'%',\
+         '＆':'&','＊':'*','＋':'+','⊙':'*','［':'[','］':']',"［":'[',"］":']',"—":'-',"·":'.',"－":'-'}
+
+
+dot = {'é','︶','『','』','ī','ō','︿','⊙','［','］'}
+
+def replace_all(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
+
+def seg(content):
+
+    content = content.encode('utf-8')
+    content = replace_all(content,Dic)
+
+    Cut=jieba.cut(''.join(content.split())) #斷詞
+
+    li = []
+    for u in Cut:
+        if u.encode('utf-8') not in dot:  #清洗全形符號
+            li.append(u) #沒在dot裡的就寫進陣列
+
+    content = (' '.join(li)).encode('utf-8') #將陣列用空白格開, 傳回字串
+
+    return content, len(li)
+
 def unicode2int(word):
     c=repr(word)
     if '\u' not in c:
@@ -78,28 +81,57 @@ def unicode2int(word):
         d= c.translate(None,r"\u'")
         return int(d,16)
 
+def transEscape(content):
+    content =re.sub(r'\n', "\\\\n", content)
+    content =re.sub(r'\"', "\"\"", content)
+    return content
+
+def transformation(content, label, wordDict):
+
+    content, seqlen = seg(content)
+    if seqlen < 10:
+        return None
+
+    content = content.decode('utf-8')
+    toks = content.split(" ")
+
+    for tok in toks:
+        for word in tok:
+            idx = unicode2int(word)
+            if word not in vocab and idx in uniset:
+                vocab.add(word)
+                wordDict.write(word.encode("utf-8") + '\n')  
+
+    pys = transfer(toks, style=style, errors=error)
+    pys = ' '.join(list(itertools.chain(*pys))).encode('utf-8')
+    pys = transEscape(pys)
+
+    exp = "\""+label+"\"" + str(',') + "\""+pys+"\""
+    return exp
+
+"""
+http://meirong. http://yule. http://sports. http://caifu. http://house.
+http://tour. http://career. http://sina. http://travel. http://auto.
+http://money. http://ent. http://mil. http://military. http://finance.
+http://tech. http://learning. http://it. http://lady. http://women.
+http://health. http://edu. http://cul. http://business. http://www.
+http://eladies. http://2008. http://fun. http://news. http://war.
+http://china. http://culture.
+"""
+
+
 if __name__ == "__main__":
 
-    totfiles = len(os.listdir(Dir))
-    pbar = ProgressBar(maxval=totfiles).start()
+    Dir = "./SogouCAS"
 
-    sport_cnt = 0
-    ent_cnt = 0
-    auto_cnt = 0
-    fin_cnt = 0
-    it_cnt = 0
-
-    num_train = 0
-    num_test = 0
+    sport_list = []
+    ent_list = []
+    auto_list = []
+    fin_list = []
+    it_list = []
 
     vocab = set()
-
-    uniset = set()
-
-    for unicode_range in unicode_ranges:
-        for n in xrange(int(unicode_range[0], 16), int(unicode_range[1], 16) + 1):
-            uniset.add(n)
-
+    
     import argparse
 
     parser = argparse.ArgumentParser(description="Usage")
@@ -114,7 +146,7 @@ if __name__ == "__main__":
     elif args.format == "py":
         transfer = pinyin
         style = pypinyin.TONE2
-        error="ignore"
+        error="default"
     elif args.format == "wb":
         transfer = wubi
         style = None
@@ -125,197 +157,111 @@ if __name__ == "__main__":
     with open("./train_"+args.format+".csv", 'w') as tr, open("./test_"+args.format+".csv", 'w') as te, \
         open("./dict.txt", 'w')as wordDict:
 
+        totfiles = len(os.listdir(Dir))
+        pbar = ProgressBar(maxval=totfiles).start()
+
         for t, name in enumerate(os.listdir(Dir)): 
             if name.startswith('.'):
                 continue
+
             time.sleep(0.01)
             pbar.update(t + 1)
 
             From = Dir + '/' + name
             with codecs.open(From, encoding='gbk', errors='ignore') as f:
                 soup = bs(f.read(),"html.parser")
-                for i, j in zip(soup.select('url'), soup.select('content')): 
 
-                    url_mention = i.text
+                for i, j, k, l in zip(soup.select('url'), soup.select('content'), 
+                    soup.select('contenttitle'), soup.select('docno')): 
+
+                    url = i.text
                     content = j.text
+                    title = k.text
+                    docno = l.text
 
-                    if content == '' or url_mention == '':
+                    if url == '' or content == '':
                         continue    
 
-                    if "http://sports." in url_mention and url_mention not in sport_url:
-                        sport_url.add(url_mention)
+                    if "http://sports." in url:
 
-                        content, seqlen = seg(content)
+                        if content != '':
 
-                        if seqlen > 30:
-                            sport_cnt += 1
-                            if sport_cnt <= 50000:
-
-                                content = content.decode('utf-8')
-                                toks = content.split(" ")
-
-                                for tok in toks:
-                                    for word in tok:
-                                        idx = unicode2int(word)
-                                        if word not in vocab and idx in uniset:
-                                            vocab.add(word)
-                                            wordDict.write(word.encode("utf-8") + '\n')  
-
-                                pys = transfer(toks, style=style, errors=error)
-
-                                pys = ' '.join(list(itertools.chain(*pys))).encode('utf-8')
-
-                                exp = "\"1\"" + str(',') + "\""+pys+"\""
-
-                                if sport_cnt % 10 !=0:
-                                    tr.write(exp+"\n")
-                                    num_train += 1
-                                else:
-                                    te.write(exp+"\n")
-                                    num_test += 1
-                                    
-
-                    elif "http://ent." in url_mention and url_mention not in ent_url:
-                        ent_url.add(url_mention)
-
-                        content, seqlen = seg(content)
-
-                        if seqlen > 30:
-                            ent_cnt += 1
-
-                            if ent_cnt <= 50000:
-
-                                content = content.decode('utf-8')
-                                toks = content.split(" ")
-
-                                for tok in toks:
-                                    for word in tok:
-                                        idx = unicode2int(word)
-                                        if word not in vocab and idx in uniset:
-                                            vocab.add(word)
-                                            wordDict.write(word.encode("utf-8") + '\n')  
-                                            
-                                pys = transfer(toks, style=style, errors=error)
-                                pys = ' '.join(list(itertools.chain(*pys))).encode('utf-8')
-                                exp = "\"2\"" + str(',') + "\""+pys+"\""
-
-                                if ent_cnt % 10 !=0:
-                                    tr.write(exp+"\n")
-                                    num_train += 1
-                                else:
-                                    te.write(exp+"\n")
-                                    num_test += 1
+                            exp = transformation(content, "1", wordDict)
+                            if exp != None:
+                                sport_list.append(exp)
 
 
-                    elif "http://auto." in url_mention and url_mention not in auto_url:
-                        auto_url.add(url_mention)
+                    elif "http://ent." in url or "http://yule." in url or "http://fun" in url:
 
-                        
-                        content, seqlen = seg(content)
+                        if content != '':
 
-                        if seqlen > 30:
-                            auto_cnt += 1
+                            exp = transformation(content, "2", wordDict)
+                            if exp != None:
+                                ent_list.append(exp)
 
-                            if auto_cnt <= 50000:
+                    elif "http://auto." in url or "auto" in url:
 
-                                content = content.decode('utf-8')
+                        if content != '':
 
-                                toks = content.split(" ")
+                            exp = transformation(content, "3", wordDict)
+                            if exp != None:
+                                auto_list.append(exp)
 
-                                for tok in toks:
-                                    for word in tok:
-                                        idx = unicode2int(word)
-                                        if word not in vocab and idx in uniset:
-                                            vocab.add(word)
-                                            wordDict.write(word.encode("utf-8") + '\n') 
+                    elif "http://finance." in url:
 
-                                pys = transfer(toks, style=style, errors=error)
-                                pys = ' '.join(list(itertools.chain(*pys))).encode('utf-8')
-                                exp = "\"3\"" + str(',') + "\""+pys+"\""
+                        if content != '':
 
-                                if auto_cnt % 10 !=0:
-                                    tr.write(exp+"\n")
-                                    num_train += 1
-                                else:
-                                    te.write(exp+"\n")
-                                    num_test += 1
-       
+                            exp = transformation(content, "4", wordDict)
+                            if exp != None:
+                                fin_list.append(exp)
 
-                    elif "http://finance." in url_mention and url_mention not in finance_url:
-                        finance_url.add(url_mention)
-                        content, seqlen = seg(content)
+                    elif "http://tech." in url or "http://it." in url:
 
-                        if seqlen > 30:
-                            fin_cnt += 1
+                        if content != '':
 
-                            if fin_cnt <= 50000:
+                            exp = transformation(content, "5", wordDict)
+                            if exp != None:
+                                it_list.append(exp)
 
-                                content = content.decode('utf-8')
+                    
+        pbar.finish()
 
-                                toks = content.split(" ")
+        num_samples = 60000
+        sports_samples = random.sample(set(sport_list), num_samples)
+        ent_samples = random.sample(set(ent_list), num_samples)
+        auto_samples = random.sample(set(auto_list), num_samples)
+        fin_samples = random.sample(set(fin_list), num_samples)
+        it_samples = random.sample(set(it_list), num_samples)
 
-                                for tok in toks:
-                                    for word in tok:
-                                        idx = unicode2int(word)
-                                        if word not in vocab and idx in uniset:
-                                            vocab.add(word)
-                                            wordDict.write(word.encode("utf-8") + '\n') 
+        all_samples = sports_samples + ent_samples + auto_samples + fin_samples + it_samples
 
-                                pys = transfer(toks, style=style, errors=error)
-                                pys = ' '.join(list(itertools.chain(*pys))).encode('utf-8')
-                                exp = "\"4\"" + str(',') + "\""+pys+"\""
+        random.shuffle(all_samples)
+        training, test = all_samples[:250000], all_samples[250000:]
 
+        for data in training:
+            tr.write(data+"\n")
 
-                                if fin_cnt % 10 !=0:
-                                    tr.write(exp+"\n")
-                                    num_train += 1
-                                else:
-                                    te.write(exp+"\n")
-                                    num_test += 1
+        for data in test:
+            te.write(data+"\n")
 
+        print '\n---------------'
+        print 'Sport_cnt',"\t",len(sport_list)
+        print 'Fin_cnt',"\t",len(fin_list)
+        print 'Ent_cnt',"\t",len(ent_list)
+        print 'Auto_cnt',"\t",len(auto_list)
+        print 'Tech_cnt',"\t",len(it_list)
 
-                    elif ("http://tech." in url_mention or "http://it." in url_mention) and url_mention not in tech_url:
-                        tech_url.add(url_mention)
-                        content, seqlen = seg(content)
-                        if seqlen > 30:
-                            it_cnt += 1
-                            if it_cnt <= 50000:
+        print 'Word Size', "\t", len(vocab)
 
-                                content = content.decode('utf-8')
+        print 'Train_cnt',"\t",len(training)
+        print 'Test_cnt',"\t",len(test)
+        print '---------------\n'
 
-                                toks = content.split(" ")
-
-                                for tok in toks:
-                                    for word in tok:
-                                        idx = unicode2int(word)
-                                        if word not in vocab and idx in uniset:
-                                            vocab.add(word)
-                                            wordDict.write(word.encode("utf-8") + '\n') 
-
-                                pys = transfer(toks, style=style, errors=error)
-                                pys = ' '.join(list(itertools.chain(*pys))).encode('utf-8')
-                                exp = "\"5\"" + str(',') + "\""+pys+"\""
-
-                                if it_cnt % 10 !=0:
-                                    tr.write(exp+"\n")
-                                    num_train += 1
-                                else:
-                                    te.write(exp+"\n")
-                                    num_test += 1
-            
-                            
-    pbar.finish()
-
-    print '\n---------------'
-    print 'Sport_cnt',"\t",sport_cnt
-    print 'Fin_cnt',"\t",fin_cnt
-    print 'Ent_cnt',"\t",ent_cnt
-    print 'Auto_cnt',"\t",auto_cnt
-    print 'Tech_cnt',"\t",it_cnt
-
-    print 'Word Size', "\t", len(vocab)
-
-    print 'Train_cnt',"\t",num_train
-    print 'Test_cnt',"\t",num_test
-    print '---------------\n'
+""" No content limitation
+Sport_cnt   571864
+Fin_cnt     141181
+Ent_cnt     259596
+Auto_cnt    70279
+Tech_cnt    73866
+"""
 
