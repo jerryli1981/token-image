095b3ce196e200c4151724549b371c3f05ddeb9d
Peng Li <jerryli1981@gmail.com>
2016-03-01 02:54:14 -0600

    change back to old optim

--- train/train.lua
+++ train/train.lua
@@ -37,8 +37,8 @@ function Train:__init(data,model,loss,config)
    self.rate = self.rates[1]
    for i,v in pairs(self.rates) do
       if i <= self.epoch and i > max_epoch then
-	 max_epoch = i
-	 self.rate = v
+         max_epoch = i
+         self.rate = v
       end
    end
 
@@ -46,8 +46,10 @@ function Train:__init(data,model,loss,config)
    self.time = {}
 
    -- Store the configurations
-   self.optim_state = config.optim_state
-   self.optimization_function = config.optim
+   --self.optim_state = config.optim_state
+   --self.optimization_function = config.optim
+   self.momentum = config.momentum
+   self.decay = config.decay
 end
 
 -- Run for a number of steps
@@ -63,7 +65,7 @@ function Train:run(epoches,logfunc)
    end
    -- The loop
    for i = 1,epoches do
-      self:batchStep_new()
+      self:batchStep()
       if logfunc then logfunc(self,i) end
    end
 end
@@ -138,64 +140,6 @@ function Train:batchStep()
    self.rate = self.rates[self.epoch] or self.rate
 end
 
-function Train:batchStep_charIdx()
-
-   self.clock = sys.clock()
-   -- Get a batch of data
-   self.batch_untyped,self.labels_untyped = self.data:getBatch(self.batch_untyped,self.labels_untyped)
-   -- Make the data to correct type
-
-   self.batch = self.batch or self.batch_untyped:type(self.model:type())
-
-   self.labels = self.labels or self.labels_untyped:type(self.model:type())
-
-   self.batch:copy(self.batch_untyped)
-
-   self.labels:copy(self.labels_untyped)
-   -- Record time
-   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
-   self.time.data = sys.clock() - self.clock
-
-   self.clock = sys.clock()
-   -- Forward propagation
-   self.output = self.model:forward(self.batch)
-   self.objective = self.loss:forward(self.output,self.labels)
-   if type(self.objective) ~= "number" then self.objective = self.objective[1] end
-   self.max, self.decision = self.output:double():max(2)
-   self.max = self.max:squeeze():double()
-   self.mask = self.labels:double():gt(0):double()
-   self.decision = self.decision:squeeze():double()
-   if self.mask:sum() > 0 then
-      self.error = torch.ne(self.decision,self.labels:double()):double():cmul(self.mask):sum()/self.mask:sum()
-   else
-      self.error = 1
-   end
-   -- Record time
-   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
-   self.time.forward = sys.clock() - self.clock
-
-   self.clock = sys.clock()
-   -- Backward propagation   
-   self.grads:zero()
-   self.gradOutput = self.loss:backward(self.output,self.labels)
-   self.gradBatch = self.model:backward(self.batch,self.gradOutput)
-   -- Record time
-   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
-   self.time.backward = sys.clock() - self.clock
-
-   self.clock = sys.clock()
-   -- Update the step
-   self.old_grads:mul(self.momentum):add(self.grads:mul(-self.rate))
-   self.params:mul(1-self.rate*self.decay):add(self.old_grads)
-   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
-   self.time.update = sys.clock() - self.clock
-
-   -- Increment on the epoch
-   self.epoch = self.epoch + 1
-   -- Change the learning rate
-   self.rate = self.rates[self.epoch] or self.rate
-end
-
 -- Run for one batch step
 function Train:batchStep_new()
 
@@ -226,8 +170,8 @@ function Train:batchStep_new()
       self.gradBatch = self.model:backward(self.batch,self.gradOutput)
 
       return self.objective, self.grads
-   end
 
+   end
 
    self.optim_state["learningRate"]=self.rate
    self.optimization_function(feval, self.params, self.optim_state)
@@ -240,6 +184,64 @@ end
 
 -- Run for one batch step
 function Train:batchStep_wb()
+
+   self.clock = sys.clock()
+   -- Get a batch of data
+   self.batch_untyped,self.labels_untyped = self.data:getBatch(self.batch_untyped,self.labels_untyped)
+
+   -- Make the data to correct type
+   self.batch = self.batch or self.batch_untyped:contiguous():type(self.model:type())
+   self.labels = self.labels or self.labels_untyped:type(self.model:type())
+   self.batch:copy(self.batch_untyped:contiguous())
+   self.labels:copy(self.labels_untyped)
+
+   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
+   self.time.data = sys.clock() - self.clock
+
+   self.clock = sys.clock()
+   -- Forward propagation
+   self.output = self.model:forward(self.batch)
+   self.objective = self.loss:forward(self.output,self.labels)
+   if type(self.objective) ~= "number" then self.objective = self.objective[1] end
+   self.max, self.decision = self.output:double():max(2)
+   self.max = self.max:squeeze():double()
+   self.mask = self.labels:double():gt(0):double()
+   self.decision = self.decision:squeeze():double()
+   if self.mask:sum() > 0 then
+      self.error = torch.ne(self.decision,self.labels:double()):double():cmul(self.mask):sum()/self.mask:sum()
+   else
+      self.error = 1
+   end
+   -- Record time
+   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
+   self.time.forward = sys.clock() - self.clock
+
+   self.clock = sys.clock()
+   -- Backward propagation   
+   self.grads:zero()
+   self.gradOutput = self.loss:backward(self.output,self.labels)
+   self.gradBatch = self.model:backward(self.batch,self.gradOutput)
+
+   -- Record time
+   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
+   self.time.backward = sys.clock() - self.clock
+
+   self.clock = sys.clock()
+   -- Update the step
+   self.old_grads:mul(self.momentum):add(self.grads:mul(-self.rate))
+   self.params:mul(1-self.rate*self.decay):add(self.old_grads)
+   if self.model:type() == "torch.CudaTensor" then cutorch.synchronize() end
+   self.time.update = sys.clock() - self.clock
+
+   -- Increment on the epoch
+   self.epoch = self.epoch + 1
+   -- Change the learning rate
+   self.rate = self.rates[self.epoch] or self.rate
+
+end
+
+-- Run for one batch step
+function Train:batchStep_wb_new()
 
    -- Get a batch of data
    self.batch_untyped,self.labels_untyped = self.data:getBatch(self.batch_untyped,self.labels_untyped)
